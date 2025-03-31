9f6cc35b86b9fd936da7086f17c899087bb81cf3
Swapnanil <135691331+swapnanil1@users.noreply.github.com>
2025-03-31 15:55:00 +0530

    directory component  is truncated to the first six characters
    six characters seems like a good balance, you may change the length as you like.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,20 @@
 function fish_prompt
+    set -l full_path (pwd)
+    set -l path_parts (string split / $full_path)
+    set -l truncated_parts
+
+    for part in $path_parts
+        if test -n "$part"
+            # disply first 6 characters; change to your like
+            set -a truncated_parts (string sub --length 6 $part)
+        else
+            # this preserved the empty parts (handeling the leading '/' correctly)
+            set -a truncated_parts ""
+        end
+    end
+    set -l display_path (string join / $truncated_parts)
     set_color 50BFB4
-    echo -n (pwd)
+    echo -n $display_path # Print the truncated path
     set_color yellow
     echo -n ' > '
     set_color normal
